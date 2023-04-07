926bef6fa23e5fa61bbdbbf1de6d8534bbd65904
samerELearning <samerelearning@outlook.com>
2023-04-07 15:09:15 +0300

    Updated the global style

--- sportsmanager/pages/index.js
+++ sportsmanager/pages/index.js
@@ -7,16 +7,15 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-      <div className={styles.home}>
-        <h2>Welcome to Sports Manager</h2>
-        <div>
-        Our platform is designed to provide a comprehensive and user-friendly solution for managing the aspects of an athletic department.
-        Our goal is to simplify the management process for athletic departments. Whether you're a high school team, a college program, or a professional organization, our platform is designed to meet your unique needs and help you achieve your goals.
-        Thank you for choosing Sports Manager as your sports management solution. We look forward to helping you succeed on and off the field!
+      <>
+        <div className={styles.home}>
+          <h2>Welcome to Sports Manager</h2>
+          <div>
+            Our platform is designed to provide a comprehensive and user-friendly solution for managing the aspects of an athletic department.
+            Our goal is to simplify the management process for athletic departments. Whether you are a high school team, a college program, or a professional organization, our platform is designed to meet your unique needs and help you achieve your goals.
+            Thank you for choosing Sports Manager as your sports management solution. We look forward to helping you succeed on and off the field!
+          </div>
         </div>
-      </div>
-
-    </>
+      </>
   )
 }
