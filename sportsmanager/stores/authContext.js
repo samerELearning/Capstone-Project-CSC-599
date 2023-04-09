4eaa3fd015339809a2b06a0156ff1e62a4b2a502
samerELearning <samerelearning@outlook.com>
2023-04-09 19:49:58 +0300

    Set event listener for login

--- sportsmanager/stores/authContext.js
+++ sportsmanager/stores/authContext.js
@@ -12,6 +12,11 @@ export const AuthContextProvider = ({ children }) => {
     const [user, setUser] = useState(null)
 
     useEffect(() => {
+        netlifyIdentity.on('login', (user) => {
+            setUser(user)
+            netlifyIdentity.close()
+            console.log('login event')
+        })
         //init netlify identity connection
         netlifyIdentity.init()
     },[])
