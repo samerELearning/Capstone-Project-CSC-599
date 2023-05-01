8ce0536e01ab24628e9f15e2470a131a2b0bbb85
samerELearning <samerelearning@outlook.com>
2023-05-01 22:56:38 +0300

    Added event scheduling solution

--- sportsmanager/pages/solutions.js
+++ sportsmanager/pages/solutions.js
@@ -1,8 +1,24 @@
 import styles from '@/styles/Solutions.module.css'
+import Link from 'next/link'
 export default function Solutions() {
     return (
       <div className={styles.solutions}>
         <h2>Our Solutions</h2>
-      </div> 
+        <ul>
+        <li className='solution'>
+          <Link href="/events">
+            <h3>Event Scheduling</h3>
+            <p>
+              With our event scheduling solution, you can easily manage games, practices, and tournaments.
+              Stay up-to-date and access your schedule from anywhere, at any time.
+              Say goodbye to the hassle of managing events and focus on what matters most - playing and enjoying the game.
+            </p>
+          </Link>
+        </li>
+        <li className='solution'><Link href="/solutions">Teams</Link>
+        </li>
+        </ul>
+      </div>
+
     )
   }
