722591607ebe8dad19b282ca8935ce1c7a5c3b41
samerELearning <samerelearning@outlook.com>
2023-05-01 23:39:18 +0300

    Added all solutions

--- sportsmanager/pages/solutions.js
+++ sportsmanager/pages/solutions.js
@@ -5,17 +5,102 @@ export default function Solutions() {
       <div className={styles.solutions}>
         <h2>Our Solutions</h2>
         <ul>
-        <li className='solution'>
+        <li className={styles.sol}>
           <Link href="/events">
-            <h3>Event Scheduling</h3>
-            <p>
-              With our event scheduling solution, you can easily manage games, practices, and tournaments.
-              Stay up-to-date and access your schedule from anywhere, at any time.
-              Say goodbye to the hassle of managing events and focus on what matters most - playing and enjoying the game.
-            </p>
+            <div className={styles.info}>
+              <h3>Event Scheduling</h3>
+              <p>
+                With our event scheduling solution, you can easily manage games, practices, and tournaments.
+                Stay up-to-date and access your schedule from anywhere, at any time.
+              </p>
+            </div>
           </Link>
         </li>
-        <li className='solution'><Link href="/solutions">Teams</Link>
+        <li className={styles.sol}>
+          <Link href="/players">
+            <div className={styles.info}>
+              <h3>Player Management</h3>
+              <p>
+              Efficiently manage your players with our player management solution.
+              Add, edit, delete, and search for players with ease.
+              </p>
+            </div>
+          </Link>
+        </li>
+        <li className={styles.sol}>
+          <Link href="/teams">
+            <div className={styles.info}>
+              <h3>Team Management</h3>
+              <p>
+              Efficiently manage your teams with our team management solution.
+              Add, edit, delete, and search for teams with ease.
+              </p>
+            </div>
+          </Link>
+        </li>
+        <li className={styles.sol}>
+          <Link href="/staff">
+            <div className={styles.info}>
+              <h3>Staff Management</h3>
+              <p>
+              Efficiently manage your staff with our staff management solution.
+              Add, edit, delete, and search for staff with ease.
+              </p>
+            </div>
+          </Link>
+        </li>
+        <li className={styles.sol}>
+          <Link href="/facilities">
+            <div className={styles.info}>
+              <h3>Facility Management</h3>
+              <p>
+              Efficiently manage your facilities with our facility management solution.
+              Add, edit, delete, and search for facilities with ease.
+              </p>
+            </div>
+          </Link>
+        </li>
+        <li className={styles.sol}>
+          <Link href="/equipments">
+            <div className={styles.info}>
+              <h3>Equipment Management</h3>
+              <p>
+              Efficiently manage your equipments with our equipment management solution.
+              Add, edit, delete, and search for equipments with ease.
+              </p>
+            </div>
+          </Link>
+        </li>
+        <li className={styles.sol}>
+          <Link href="/uniforms">
+            <div className={styles.info}>
+              <h3>Uniform Management</h3>
+              <p>
+              Efficiently manage your uniforms with our uniform management solution.
+              Add, edit, delete, and search for uniforms with ease.
+              </p>
+            </div>
+          </Link>
+        </li>
+        <li className={styles.sol}>
+          <Link href="/import">
+            <div className={styles.info}>
+              <h3>Data Import</h3>
+              <p>
+              Say goodbye to manual data entry - import all of your player information quickly and easily with just a few clicks.
+              </p>
+            </div>
+          </Link>
+        </li>
+        <li className={styles.sol}>
+          <Link href="/export">
+            <div className={styles.info}>
+              <h3>Data Export</h3>
+              <p>
+              Effortlessly export data with our data export solution. Download data and take it wherever you need.
+              </p>
+            </div>
+          </Link>
         </li>
         </ul>
       </div>
