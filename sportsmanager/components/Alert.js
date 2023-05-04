bebefced8d7f576f156b66ce18e5ca0ba3b08346
samerELearning <samerelearning@outlook.com>
2023-05-04 11:02:08 +0300

    Implemented Alert component

---
+++ sportsmanager/components/Alert.js
@@ -0,0 +1,12 @@
+export default function Alert() {
+    return (
+        <>
+        <div class="alert w-50 align-self-center alert-success alert-dismissible fade show" role="alert">
+			Data Added successfully
+			<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+			  <span aria-hidden="true">&times;</span>
+			</button>
+		</div>
+        </>
+    )
+}
