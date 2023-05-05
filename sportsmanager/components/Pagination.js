101bad640a339a1f45584800e8ea846064df8d5a
samerELearning <samerelearning@outlook.com>
2023-05-05 09:08:39 +0300

    Implemented Pagination component

---
+++ sportsmanager/components/Pagination.js
@@ -0,0 +1,14 @@
+export default function Pagination() {
+    return (
+        <>
+        <div class="d-flex justify-content-center align-items-center">
+				<ul class="pagination">
+					<li class = "page-item active"><a href="#" class = "page-link">1</a></li>
+					<li class = "page-item"><a href="#" class = "page-link">2</a></li>
+					<li class = "page-item"><a href="#" class = "page-link">3</a></li>
+					<li class = "page-item"><a href="#" class = "page-link">4</a></li>
+				</ul>
+		</div>
+        </>
+    )
+}
