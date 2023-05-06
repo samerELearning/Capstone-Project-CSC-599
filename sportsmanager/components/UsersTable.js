75d7c6cdff66940b3921abd9b65915346e71578c
samerELearning <samerelearning@outlook.com>
2023-05-06 16:28:05 +0300

    Implemented user table model

--- sportsmanager/components/UsersTable.js
+++ sportsmanager/components/UsersTable.js
@@ -12,6 +12,10 @@ export default function UsersTable() {
 						</th>
 						<th>Name</th>
 						<th>Email</th>
+						<th>Id</th>
+						<th>Sport</th>
+						<th>Team</th>
+						<th>Size</th>
 						<th>Actions</th>
 					</tr>
 				</thead>
@@ -24,8 +28,12 @@ export default function UsersTable() {
 								<label for="data_checkbox"></label>
 							</span>
 						</td>
-						<td>Anand Raj</td>
-						<td>Rajanand9039@gmail.com</td>
+						<td>Student1</td>
+						<td>Student1@lau.edu</td>
+						<td>202001460</td>
+						<td>basketball</td>
+						<td>A</td>
+						<td>M</td>
 						<td>
 							<a href="#editEmployeeModal" className="edit" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
 							<a href="#deleteEmployeeModal" className="delete" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
@@ -39,8 +47,12 @@ export default function UsersTable() {
 								<label for="data_checkbox"></label>
 							</span>
 						</td>
-						<td>Anand Raj</td>
-						<td>Rajanand@gmail.com</td>
+						<td>Student2</td>
+						<td>Student2@lau.edu</td>
+						<td>202101460</td>
+						<td>Table Tennis</td>
+						<td>A</td>
+						<td>S</td>
 						<td>
 							<a href="#editEmployeeModal" className="edit" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
 							<a href="#deleteEmployeeModal" className="delete" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
@@ -54,8 +66,12 @@ export default function UsersTable() {
 								<label for="data_checkbox"></label>
 							</span>
 						</td>
-						<td>Anand Raj</td>
-						<td>Rajanand@gmail.com</td>
+						<td>Student3</td>
+						<td>Student3@lau.edu</td>
+						<td>202302450</td>
+						<td>Football</td>
+						<td>A</td>
+						<td>L</td>
 						<td>
 							<a href="#editEmployeeModal" className="edit" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
 							<a href="#deleteEmployeeModal" className="delete" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
@@ -69,8 +85,12 @@ export default function UsersTable() {
 								<label for="data_checkbox"></label>
 							</span>
 						</td>
-						<td>Anand Raj</td>
-						<td>Rajanand@gmail.com</td>
+						<td>Student4</td>
+						<td>Student4@lau.edu</td>
+						<td>201439450</td>
+						<td>basketball</td>
+						<td>B</td>
+						<td>XL</td>
 						<td>
 							<a href="#editEmployeeModal" className="edit" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
 							<a href="#deleteEmployeeModal" className="delete" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
