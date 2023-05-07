4cdb687b30c4ea0aa93c2880d097da4c90fd1d1d
samerELearning <samerelearning@outlook.com>
2023-05-07 15:46:00 +0300

    Implemented POST api for user

--- sportsmanager/pages/api/users/index.js
+++ sportsmanager/pages/api/users/index.js
@@ -5,6 +5,8 @@ export default async function handler(req, res) {
         
         case "GET" :
             return await getUser(req, res);
+        case "POST":
+            return await addUser(req, res);
     }
 }
 
@@ -14,6 +16,24 @@ const getUser = async (req, res) => {
 
         const result = await prisma.user.findMany();
     }catch(error){
-        return res.status(200).json(error);
+        return res.status(500).json(error);
+    }
+}
+
+const addUser = async (req, res) => {
+
+    try {
+        const data = {
+            username : "Samer",
+            email    : "SamerSaber@hotmail.com"
+        }
+
+        const result = await prisma.user.create({
+            data : data
+        })
+
+        return res.status(200).json({success : true});
+    }catch(error){
+        return res.status(500).json(error);
     }
 }
